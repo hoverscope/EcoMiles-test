2a7fb865f2c80fcb395d7862b840a5b928d085c1
hoverscope <96564653+hoverscope@users.noreply.github.com>
2025-04-02 21:27:12 +0400

    router change

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'  // Import necessary components from react-router-dom
-import './index.css'
-import App from './App.tsx'
-import Demo from './app/pages/demo.tsx'
-import Rewards from './app/pages/rewards.tsx'
-
-
-
-
-
-
-
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+import Demo from './app/pages/demo.tsx';
+import Rewards from './app/pages/rewards.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/demo" element={<Demo />} />
         <Route path="/rewards" element={<Rewards />} />
-
-
-        
-     
-
-
-
-
-
-
-
       </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    </HashRouter>
+  </StrictMode>
+);
